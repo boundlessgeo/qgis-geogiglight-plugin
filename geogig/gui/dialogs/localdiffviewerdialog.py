9cb59e28ef7aabbad21dd43d4130da731f63ec2d
Alexandre Neto <senhor.neto@gmail.com>
2017-04-18 15:54:46 +0100

    Convert some Icons to SVG format

--- geogig/gui/dialogs/localdiffviewerdialog.py
+++ geogig/gui/dialogs/localdiffviewerdialog.py
@@ -56,7 +56,7 @@ from geogig.tools.layertracking import getTrackingInfo
 
 MODIFIED, ADDED, REMOVED = "M", "A", "R"
 
-layerIcon = QIcon(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ui", "resources", "layer_group.gif"))
+layerIcon = QIcon(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ui", "resources", "layer_group.svg"))
 featureIcon = QIcon(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ui", "resources", "geometry.png"))
 addedIcon = QIcon(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ui", "resources", "added.png"))
 removedIcon = QIcon(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "ui", "resources", "removed.png"))
